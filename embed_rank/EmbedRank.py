5edf7be4224d6a13d050dd734751e2e227745955
JiangHe <jiangh0124@gmail.com>
2020-09-21 19:12:05 -0400

    edited init file to download nltk data if not exist

--- embed_rank/EmbedRank.py
+++ embed_rank/EmbedRank.py
@@ -3,11 +3,31 @@ import nltk
 import unidecode
 from collections import defaultdict
 import re
+import contractions
 
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('wordnet')
+stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 
+            'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 
+            'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 
+            "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 
+            'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 
+            'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 
+            'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 
+            'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 
+            'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 
+            'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 
+            'through', 'during', 'before', 'after', 'above', 'below', 'to', 
+            'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 
+            'again', 'further', 'then', 'once', 'here', 'there', 'when', 
+            'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 
+            'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 
+            'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 
+            'can', 'will', 'just', 'don', "don't", 'should', "should've", 
+            'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', 
+            "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', 
+            "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 
+            'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 
+            'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', 
+            "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
 class EmbedRank:
     '''
@@ -32,7 +52,7 @@ class EmbedRank:
     def __init__(self):
         self.sent_tokenizer = nltk.tokenize.sent_tokenize
         self.punct_tokenizer = nltk.RegexpTokenizer(r"[^\W_]+|[^\W_\s]+")
-        self.stop_words = set(nltk.corpus.stopwords.words('english'))
+        self.stop_words = stopwords
         self.pos_tagger = nltk.pos_tag
         self.chucker = nltk.RegexpParser("""NP:{<JJ>*<NN.*>{0,3}}  # Adjectives (0) plus Nouns (1-3)""")
         self.lemmatizer = nltk.stem.WordNetLemmatizer()
--- embed_rank/__init__.py
+++ embed_rank/__init__.py
@@ -1,1 +1,22 @@
+'''
+This file downloads 
+'''
+import nltk
 
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    print("Downloading NLTK Tokenizer\n")
+    nltk.download("punkt")
+
+try:
+    nltk.data.find('taggers/averaged_perceptron_tagger')
+except LookupError:
+    print("Downloading NLTK Averaged Perceptron Tagger")
+    nltk.download('averaged_perceptron_tagger\n')
+
+try:
+    nltk.data.find('corpora/wordnet')
+except LookupError:
+    print("Downloading NLTK WordNet\n")
+    nltk.download('wordnet')
