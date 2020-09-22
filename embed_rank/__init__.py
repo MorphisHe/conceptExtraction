33a37a8f23b8f16958a691505817c70decf315cc
JiangHe <jiangh0124@gmail.com>
2020-09-21 21:04:00 -0400

    added enviornment for tika

--- embed_rank/__init__.py
+++ embed_rank/__init__.py
@@ -2,7 +2,12 @@
 This file downloads 
 '''
 import nltk
+import os
 
+# set-up enviornment for off line tika server
+os.environ['TIKA_SERVER_JAR'] = os.getcwd() + "/embed_rank/tika_server/tika-server-1.24.1.jar"
+
+# check nltk data avaliability. Download if not avaliable 
 try:
     nltk.data.find('tokenizers/punkt')
 except LookupError:
@@ -20,3 +25,5 @@ try:
 except LookupError:
     print("Downloading NLTK WordNet\n")
     nltk.download('wordnet')
+
+
